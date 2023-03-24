821c16a0e65fd38aa0ac1cb490e78947ad829676
Landon Zafiropoulo <58679340+LandonCodes@users.noreply.github.com>
2023-03-24 10:51:24 -0700

    added to main

--- main.py
+++ main.py
@@ -11,75 +11,69 @@ from setting import *
 from sprites import *
 from random import randint
 # from pg.sprite import Sprite
+class Game:
+    def __int__(self):
+        pg.init()
+        pg.mixer.init()
+        self.scree = pg.display.set_mode((WIDTH, HEIGHT))
+        pg.display.set_caption("MY GAME")
+        self.clock = pg.time.Clock()
+        self.running = True
+    
+    def new(self):
+        self.score = 0
+        self.all_sprites = pg.sprite.Group()
+        self.platforms = pg.sprite.Group()
+        self.enemies = pg.sprite.Group()
+        self.player = Player(self)
+        self.all_sprites.add(self.player)
+        for i in range(1,10):
+            e = Mob()
+            self.all_sprites.add(e)
+        self.run()
+    def run(self):
+        self.playing = True
+        while self.playing:
+            self.clock.tick(FPS)
+            self.events()
+            self.update()
+            self.draw()
+    def events(self):
+        # Game Loop - events
+        for event in pg.event.get():
+            # check for closing window
+            if event.type == pg.QUIT:
+                if self.playing:
+                    self.playing = False
+                self.running = False
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_SPACE:
+                    self.player.jump()
+    def draw_text(self, text, size, color, x, y):
+        font_name = pg.font.match_font('arial')
+        font = pg.font.Font(font_name, size)
+        text_surface = font.render(text, True, color)
+        text_rect = text_surface.get_rect()
+        text_rect.midtop = (x, y)
+        self.screen.blit(text_surface, text_rect)
+    # def get_mouse_now():
+    #     x,y = pg.mouse.get_pos()
+    #     return (x,y)
+    def update(self):
+        self.all_sprites.update()
+    def draw(self):
+        self.screen.fill(BLUE)
+        self.draw_text("Hello there!", 42, WHITE, WIDTH/2, HEIGHT/10)
+        self.all_sprites.draw(self.screen)
+        pg.display.flip()
+
 vec = pg.math.Vector2
-
-# set up assets folders
-game_folder = os.path.dirname(__file__)
-img_folder = os.path.join(game_folder, "img")
-
-def get_mouse_now():
-    x,y = pg.mouse.get_pos()
-    return (x,y)
-
-
-# init pg and create window
-pg.init()
-# starts or initalizes the sound mixer
-pg.mixer.init()
-screen = pg.display.set_mode((WIDTH, HEIGHT))
-pg.display.set_caption("my first game")
-clock = pg.time.Clock() 
-
-player_img = pg.image.load(path.join(img_folder, "bell-ar-man.png")).convert()
-
-all_sprites = pg.sprite.Group()
-enemies = pg.sprite.Group()
-# defines player as player
-player = Player(player_img)
-invader = Mob()
-invader.image.fill((0,0,255))
-invader.vel = vec(randint (1,8), randint (8,10))
-
-for i in range(0,10):
-    m = Mob()
-    m.vel = vec(randint (1,8), randint (8,10))
-    all_sprites.add(m)
-    enemies.add(m)
-
-                 
-# testSprite = Sprite()
-# testSprite.image = pg.Surface((50,50))
-# testSprite.image.fill(GREEN)
-# testSprite.rect = testSprite.image.get_rect()
-# testSprite.rect.center = (WIDTH / 2, HEIGHT / 2)
-all_sprites.add(player)
-all_sprites.add(invader)
-# all_sprites.add(testSprite)
 
 # game loop
 
-while RUNNING:
-    #  keep loop running at the right speed
-    clock.tick(FPS)
-    ### process input events section of game loop
-    for event in pg.event.get():
-        # check for window closing
-        if event.type == pg.QUIT:
-            RUNNING = False
-            # break
-    # print(get_mouse_now())
-    all_sprites.update()
+g = Game()
 
-    blocks_hit_list = pg.sprite.spritecollide(player, enemies, False)
-    for block in blocks_hit_list:
-        print(enemies)
-        pass
-    # draw and render section of game loop
-    screen.fill(BLUE)
-    all_sprites.draw(screen)
-    screen.blit(player_img, player.rect)
-    # double buffering draws frames for entire screen
-    pg.display.flip()
-    # pg.display.update() -> only updates a portion of the screen
-# ends program when loops evaluates to false
+while g.running:
+    g.new()
+
 pg.quit()
