f7d858bd53040de740d5531866a990662f53024c
Landon Zafiropoulo <58679340+LandonCodes@users.noreply.github.com>
2023-03-30 14:35:27 -0700

    player eat enemies

--- setting.py
+++ setting.py
@@ -7,17 +7,24 @@ HEIGHT = 600
 ############# game settings ##############
 FPS = 30
 RUNNING = True
+SCORE = 0
+PAUSED = False
 
 ############ player attributes ###########
 PLAYER_ACC  = 2
 PLAYER_FRICTION = -0.12
 MOB_FRICTION = -0.7
 MOB_ACC= 0.7
-PLAYER_GRAV = 1.5
+PLAYER_GRAV = 1
 PLAYER_JUMP = 20
 ################ colors ##################
 BLACK = (0,0,0)
-BLUE = (50, 50, 255)
-RED = (200,0,0)
 WHITE = (255,255,255)
-GREEN = (0,255,0)
+BLUE = (50,50,255)
+RED = (255,50,50)
+########## Starting platforms ############
+PLATFORM_LIST = [(0, HEIGHT - 40, WIDTH, 40, (200,200,200), "normal"),
+                 (WIDTH / 2 - 50, HEIGHT * 3 / 4, 100, 20, (100,255,100), "bouncey"),
+                 (125, HEIGHT - 350, 100, 5, (200,100,50), "disappearing"),
+                 (350, 200, 100, 20, (200,200,200), "normal"),
+                 (175, 100, 50, 20, (200,200,200), "normal")]
